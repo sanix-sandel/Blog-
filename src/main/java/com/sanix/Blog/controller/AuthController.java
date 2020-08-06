e57129eedd24c1c09efdadf4b6cf02ea080f3191
sanix sandel <sanicksikani@gmail.com>
2020-08-06 05:03:14 +0300

    signed up

--- src/main/java/com/sanix/Blog/controller/AuthController.java
+++ src/main/java/com/sanix/Blog/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.sanix.Blog.controller;
 
 
+import com.sanix.Blog.dto.RegisterRequest;
+import com.sanix.Blog.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
--- src/main/java/com/sanix/Blog/repository/UserRepository.java
+++ src/main/java/com/sanix/Blog/repository/UserRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);//retrieve the user by username
+    Optional<User> findByUserName(String username);//retrieve the user by username
 }
